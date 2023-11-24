a4670cf71ae129cfea2aa8d59b1a2431f7c730ec
Alexander Pushkin (Home) <a.v.pushkin@gmail.com>
2023-11-24 14:44:25 +0300

    upd

--- etsy/src/components/level.tsx
+++ etsy/src/components/level.tsx
@@ -1,15 +1,15 @@
 function getQuantityLevel(quantity: number): string {
     if (quantity <= 10) {
-        return " level-low";
+        return "level-low";
     } else if (quantity <= 20) {
-        return " level-medium";
+        return "level-medium";
     } else {
-        return " level-high";
+        return "level-high";
     }
 }
 
 export function Level({ qty }: { qty: number }): React.ReactNode {
     return (
-        <p className={"item-quantity" + getQuantityLevel(qty)}>{qty}</p>
+        <p className={"item-quantity" + " " + getQuantityLevel(qty)}>{qty}</p>
     )
 }
